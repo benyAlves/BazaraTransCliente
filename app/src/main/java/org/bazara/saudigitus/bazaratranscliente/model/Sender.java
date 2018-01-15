0c2be86b808cb57faf5169c4ce8c09a49e49ecee
beny <malulequeb@gmail.com>
2018-01-15 09:50:32 +0200

    sender class para notificacao

---
+++ app/src/main/java/org/bazara/saudigitus/bazaratranscliente/model/Sender.java
@@ -0,0 +1,17 @@
+package org.bazara.saudigitus.bazaratranscliente.model;
+
+/**
+ * Created by SD on 1/15/2018.
+ */
+
+public class Sender {
+
+    public Data data;
+    private String destination;
+
+
+    public Sender() {
+    }
+
+
+}
